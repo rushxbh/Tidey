046d8044798439907000e7ca353c761ff1a5d847
rushxbh <rushabh9372@gmail.com>
2025-06-27 11:16:28 +0530

    blockchain

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -13,7 +13,7 @@ function AuthLayout() {
             <span className="ml-2 text-2xl font-bold text-primary-500">Tidewy</span>
           </Link>
           <h2 className="mt-6 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-            Welcome to Tidewy
+            Welcome to Tidey
           </h2>
         </div>
 
